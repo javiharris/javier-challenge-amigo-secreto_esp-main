cd4d09f3e3ca1579679f234bdbe787ac8153cd87
javier harris aguirre <javierharris@gmail.com>
2025-03-07 14:01:00 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+// Función para hacer el sorteo de amigo secreto
+function sortearAmigo() {
+  if (nombres.length === 0) {
+      alert("No hay nombres en la lista para sortear. Agrega al menos uno.");
+      return;
+  }
+
